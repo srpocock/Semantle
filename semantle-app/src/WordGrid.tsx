ed77865479b2fe3d0cd947f217b51f5127e78b9d
Simon Pocock <32938450+srpocock@users.noreply.github.com>
2025-07-20 11:36:08 +0100

    Some initial styling

--- semantle-app/src/WordGrid.tsx
+++ semantle-app/src/WordGrid.tsx
@@ -1,23 +1,29 @@
+import './styles/WordGrid.css'
 import { GameState, type GameStateType } from './GameState';
 
 type GridProps = { gameState: GameStateType, words: string[], checkedWords: string[], onChecked: (word: string, checked: boolean) => void };
 type WordCardProps = { word: string, enabled: boolean, checked: boolean, onChecked: (word: string, checked: boolean) => void };
 
 function WordCard({ word, enabled, checked, onChecked }: WordCardProps) {
+
+    
+
     return (
-        <label className="word-tile" data-status="untested">
-            <input type="checkbox" checked={checked} disabled={!enabled} onChange={e => onChecked(word, e.target.checked)} />
-            {word}
-        </label>
+        <div className="word-tile-container">
+            <label className="word-tile" data-status="untested">
+                <input type="checkbox" checked={checked} disabled={!enabled} onChange={e => onChecked(word, e.target.checked)} />
+                {word}
+            </label>
+        </div>
     );
 }
 
 export default function WordGrid({ gameState, words, checkedWords, onChecked }: GridProps) {
     return (
-        <>
+        <fieldset className="word-grid">
             {words.map((word, index) => (
                 <WordCard key={index} word={word} enabled={gameState === GameState.Playing} checked={checkedWords.includes(word)} onChecked={onChecked} />
             ))}
-        </>
+        </fieldset>
     );
 }
